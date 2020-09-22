028d2a3a9774a229a2179f1aff29cd8ec76d0878
Jon Schmedt <jon.schmedt@usfoods.com>
2020-09-22 14:10:04 -0500

    change on feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there dude! Make thing's simple with Docker.
+          Hi there dude! Make thing's simple with Docker. I was changed on the feature branch.
         </p>
         <a
           className="App-link"
